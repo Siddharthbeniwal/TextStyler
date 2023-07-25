836de93410850dc7b9d2e0fb78dddfb9e8e24417
Siddharthbeniwal <siddharthben16@gmail.com>
2023-07-25 23:33:47 +0530

    Revert last changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import BrowserRouter from  'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
-  <BrowserRouter basename="TextStyler">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
