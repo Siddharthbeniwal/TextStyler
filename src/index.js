1daffc8d04e7b06ea692e9885d80b635d58a92b2
Siddharthbeniwal <siddharthben16@gmail.com>
2023-07-25 21:42:23 +0530

    BrowserRouter changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import BrowserRouter from  'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename="TextStyler">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
